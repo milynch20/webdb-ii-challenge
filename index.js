91c04edc882236a7972575a6b09f5a950f81fcfe
Malcolm Lynch <milynch1320@gmail.com>
2019-06-21 15:19:07 -0500

    MVP Complete

--- index.js
+++ index.js
@@ -64,6 +64,38 @@ server.post('/api/zoos', (req, res) => {
     });
 });
 
+server.put('/api/zoos/:id', (req, res) => {
+  db('zoos')
+    .where({ id: req.params.id })
+    .update(req.body)
+    .then(count => {
+      if (count > 0) {
+        res.status(200).json({message: `${count} ${count > 1 ? 'records' : 'record'} updated`,});
+      } else {
+        res.status(404).json({message: 'zoo does not exist '})
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+server.delete('/api/zoos/:id', (req, res) => {
+  db('zoos')
+  .where({id: req.params.id})
+  .del()
+  .then(count =>{
+    if(count > 0) {
+      res.status(200).json({message: `${count} ${count > 1 ? 'records' : 'record'} updated`,});
+    } else {
+      res.status(404).json({message: 'Role does not exist '})
+    }
+  })
+  .catch(err => {
+    res.status(500).json(err);
+  });
+});
+
 
 const port = 3300;
 server.listen(port, function() {
